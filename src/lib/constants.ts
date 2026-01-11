10a5a07c494d372a31dea878ae7d7d708ab2d46b
Weslley Monteiro <weslleymont909@gmail.com>
2026-01-11 03:54:50 +0000

    86988413395 muda pra esse numero de novo

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -1,3 +1,3 @@
 // Please use the format with country code, without '+', spaces or hyphens.
 // Example: 5511999999999
-export const WHATSAPP_PHONE_NUMBER = '5521965517467';
+export const WHATSAPP_PHONE_NUMBER = '5586988413395';
