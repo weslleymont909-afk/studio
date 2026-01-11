1f2a18cc047c9fc1916627f443041fb527af4efa
Weslley Monteiro <weslleymont909@gmail.com>
2026-01-11 13:30:06 +0000

    parece qque não funcionou

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -1,3 +1,3 @@
 // Please use the format with country code, without '+', spaces or hyphens.
 // Example: 5511999999999
-export const WHATSAPP_PHONE_NUMBER = process.env.NEXT_PUBLIC_WHATSAPP_PHONE_NUMBER || '21965517467';
+export const WHATSAPP_PHONE_NUMBER = process.env.NEXT_PUBLIC_WHATSAPP_PHONE_NUMBER || '';
